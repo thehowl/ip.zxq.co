98199bca4e377059ccc0228e60f5c7ed40f793e6
Howl <git@howl.moe>
2015-09-20 22:38:34 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"github.com/oschwald/geoip2-golang"
+	"log"
+	"net"
+)
+
+func main() {
+	// Initialize the database.
+	db, err := geoip2.Open("GeoLite2-City.mmdb")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Whatever we do, we should always close the database.
+	defer db.Close()
+	var i string
+	// Tell the user to type in an IP.
+	fmt.Print("Type in an IP... ")
+	// Get the IP from stdin
+	_, err = fmt.Scanf("%s", &i)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Parse the ip.
+	ip := net.ParseIP(i)
+	if ip == nil {
+		fmt.Println("That's not an IP, you dumb fuck!")
+		return
+	}
+	record, err := db.City(ip)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Print out the ISO code of the country.
+	fmt.Printf("%s\n", record.Country.IsoCode)
+}
