d181754b277492a3b227c2afd44d361cb22dd3d6
Howl <git@howl.moe>
2015-09-21 17:38:41 +0200

    Add support for second parameter (like ipinfo.io's)

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 	fmt.Println("Server listening!")
 	http.ListenAndServe(":8080", nil)
 }
-func IPToResponse(i string) (string, string) {
+func IPToResponse(i string, specific string) (string, string) {
 	// Parse the ip.
 	ip := net.ParseIP(i)
 	if ip == nil {
@@ -37,13 +37,37 @@ func IPToResponse(i string) (string, string) {
 	if record.Subdivisions != nil {
 		sd = record.Subdivisions[0].Names["en"]
 	}
-	// Print out the ISO code of the country.
-	output, _ := json.Marshal(map[string]interface{}{"ip": ip, "country": record.Country.IsoCode, "country_full": record.Country.Names["en"], "city": record.City.Names["en"], "region": sd, "continent": record.Continent.Code, "continent_full": record.Continent.Names["en"], "postal": record.Postal.Code, "loc": fmt.Sprintf("%.4f,%.4f", record.Location.Latitude, record.Location.Longitude)})
-	return string(output[:]), "application/json"
+	data := map[string]string{"ip": ip.String(), "country": record.Country.IsoCode, "country_full": record.Country.Names["en"], "city": record.City.Names["en"], "region": sd, "continent": record.Continent.Code, "continent_full": record.Continent.Names["en"], "postal": record.Postal.Code, "loc": fmt.Sprintf("%.4f,%.4f", record.Location.Latitude, record.Location.Longitude)}
+	if specific == "" || specific == "json" || specific == "geo" {
+		bytes_output, _ := json.Marshal(data)
+		return string(bytes_output[:]), "application/json"
+	} else if val, ok := data[specific]; ok {
+		return val, "text/html"
+	} else {
+		return "undefined", "text/html"
+	}
 }
 func HTTPRequestHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("[rq]", r.Method, r.URL.Path)
-	o, contentType := IPToResponse(strings.Split(r.URL.Path, "/")[1])
+	requestedThings := strings.Split(r.URL.Path, "/")
+	var IPAddress string
+	var Which string
+	// How in the world the user would manage to even send a request to
+	// something without even having Path = "/"?
+	// I... have no idea. But I'm paranoid. So let's just do it anyway.
+	if len(requestedThings) < 2 {
+		IPAddress = ""
+	} else {
+		IPAddress = requestedThings[1]
+	}
+	// In case the user didn't write a specific index, let's specify it for
+	// them.
+	if len(requestedThings) < 3 {
+		Which = ""
+	} else {
+		Which = requestedThings[2]
+	}
+	o, contentType := IPToResponse(IPAddress, Which)
 	w.Header().Set("Content-Type", contentType+"; charset=utf-8")
 	fmt.Fprint(w, o)
 }
